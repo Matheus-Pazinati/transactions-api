b2f5e8c78dbcd3d7ebcd692c9664c3c5958a3853
Matheus Pazinati <matheuspazinati@gmail.com>
2023-04-17 08:32:33 -0300

    Cria rota post para criar uma nova transacao

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,25 @@
 import { FastifyInstance } from "fastify"
 import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const transactions = knex('sqlite_schema').select('*')
+  app.post('/', async (request, reply) => {
+    const TransactionRequestBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debt'])
+    })
+
+    const { title, amount, type } = TransactionRequestBodySchema.parse(request.body) 
+
+    await knex('transactions')
+    .insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1
+    })
   
-    return transactions
+    return reply.status(201).send()
   })
 }
