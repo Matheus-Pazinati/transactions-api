58758af740bc2082734edc1aeaad493bdc9f009d
Matheus Pazinati <matheuspazinati@gmail.com>
2023-04-17 07:55:05 -0300

    Cria plugin da rota de transacoes

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,5 @@
 import { knex as setupKnex, Knex } from 'knex'
-import { env } from './.env/index'
+import { env } from './env/index'
 
 export const knexConfig: Knex.Config = {
   client: 'sqlite',
