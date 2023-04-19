144289a05bd76af4e3056a98553f2aeba0e71a56
Matheus Pazinati <matheuspazinati@gmail.com>
2023-04-19 20:52:31 -0300

    Armazena em um cookie o session-id para identificar um usuario na aplicacao

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { env } from './env';
+import cookies from '@fastify/cookie'
 import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
+app.register(cookies)
 app.register(transactionsRoutes, {
   prefix: 'transactions'
 })
