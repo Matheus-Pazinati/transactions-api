9275ac676ad6ec553f9903633d1386747632c6be
Matheus Pazinati <matheuspazinati@gmail.com>
2023-04-21 22:37:24 -0300

    Cria middleware para verificar se o usuario possui um session-id salvo nos Cookies

---
+++ src/middlewares/check-session-id.ts
@@ -0,0 +1,11 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function checkSessionId(request: FastifyRequest, reply: FastifyReply) {
+  const { sessionId } = request.cookies
+
+  if (!sessionId) {
+    reply.status(401).send({
+      error: 'Unauthorized'
+    })
+  }
+}
