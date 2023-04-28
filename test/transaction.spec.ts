993b7888429afa0a661d77f9462ef92906914ff7
Matheus Pazinati <matheuspazinati@gmail.com>
2023-04-27 22:33:01 -0300

    Categoriza os testes utilizando o metodo describe

--- test/transaction.spec.ts
+++ test/transaction.spec.ts
@@ -1,22 +1,25 @@
-import { test, beforeAll, afterAll } from 'vitest'
+import { describe, it, beforeAll, afterAll } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll( async () => {
-  await app.ready()
+describe('Transactions routes', () => {
+  beforeAll( async () => {
+    await app.ready()
+  })
+
+  afterAll( async () => {
+    await app.close()
+  })
+
+  it('should create new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Air Fryer',
+        amount: 400,
+        type: 'debt'
+      })
+      .expect(201)
+  })
 })
 
-afterAll( async () => {
-  await app.close()
-})
-
-test('should create new transaction', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'Air Fryer',
-      amount: 400,
-      type: 'debt'
-    })
-    .expect(201)
-})
