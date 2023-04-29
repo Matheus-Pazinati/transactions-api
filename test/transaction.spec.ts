beb236184a29d078c71d5f6c6f675e615e5f4fb3
Matheus Pazinati <matheuspazinati@gmail.com>
2023-04-29 09:09:04 -0300

    Cria teste para listar uma unica transacao com base no seu id

--- test/transaction.spec.ts
+++ test/transaction.spec.ts
@@ -49,7 +49,35 @@ describe('Transactions routes', () => {
         amount: 400
       })
     ])
+  })
 
+  it ('should list a transaction by id', async () => {
+    const createTransactionResponse = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'Air Fryer',
+      amount: 400,
+      type: 'credit'
+    })
+
+    const cookies = createTransactionResponse.headers['set-cookie']
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set("Cookie", cookies)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const transactionResponse = await request(app.server)
+    .get(`/transactions/${transactionId}`)
+    .set("Cookie", cookies)
+
+    expect(transactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'Air Fryer',
+        amount: 400
+      })
+    )
   })
 })
 
